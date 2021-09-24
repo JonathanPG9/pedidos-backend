b6ad00b9ff0fcbc53fa83f8c39e84a4e21408d85
Jonathan Paniagua <jonatanpaniagua21@gmail.com>
2021-09-23 23:24:37 -0300

    fix

--- middleware/handleError.js
+++ middleware/handleError.js
@@ -3,7 +3,7 @@ const manejoDeErrores = {
   ValidationError : (res,error) => res.status(401).send(`${error}`).end(),
   CastError : (res,error) => res.status(400).send(`El usuario no está registrado`).end(),
   SyntaxError: (res,error) => res.status(498).send(`${error}`).end(),
-  TypeError : res => res.status(404).send("Datos erroneos").end(),
+  TypeError : res => res.status(404).send("Email y/o password invalidos").end(),
   default : res => res.status(500).send("Error interno").end()
 }
 module.exports = ((error,req,res,next) => {
